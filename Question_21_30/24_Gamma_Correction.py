3c53ce5ee5c7d965a62ae506c341fb102b63d552
KuKuXia <liuxiaolongsdu@mail.sdu.edu.cn>
2019-05-30 18:21:34 +0800

    Finished Problem 21-27.

--- Question_21_30/24_Gamma_Correction.py
+++ Question_21_30/24_Gamma_Correction.py
@@ -1,22 +1,21 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 # Read image
-img = cv2.imread("imori_gamma.jpg").astype(np.float)
-
-# Gammma correction
+img = cv2.imread("./imori_gamma.jpg").astype(np.float)
+# Gamma correction
 c = 1.
 g = 2.2
 
 out = img.copy()
-out /= 255.
-out = (1/c * out) ** (1/g)
+out /= 255
+out = (1 / c * out) ** (1 / g)
 
 out *= 255
 out = out.astype(np.uint8)
 
 # Save result
-cv2.imshow("result", out)
+
+cv2.imshow("Gamma Corrected", out)
 cv2.waitKey(0)
 cv2.imwrite("out.jpg", out)
